fa03688916cbc6a9fc612bd11af7824728672dff
Soheil Mehrjoo <soheilmehrjoo1379@gmail.com>
2024-04-26 09:12:08 +0330

    changed forms input to mui

--- components/shared/formInputs/FileInput.tsx
+++ components/shared/formInputs/FileInput.tsx
@@ -1,88 +1,64 @@
-import React from 'react';
+import React from "react";
+import { FormControl, InputAdornment, InputLabel, TextField } from "@mui/material";
+import { useController } from "react-hook-form";
+import { UploadCloud01 as CloudUploadOutlinedIcon } from "@untitled-ui/icons-react";
 
 interface Props {
-   id: any;
-   name?: string;
-   label?: string;
-   type?: string;
-   value: string;
-   onChange: any;
-   clear?: any;
-   required?: boolean;
-   touched: any;
-   onBlur: any;
-   error?: any;
-   col?: number;
-   isDisabled?: boolean;
-   multiple?: boolean;
-   customOnChange?: any;
+  id: string;
+  label: string;
+  control: any;
+  name: string;
+  error: any;
+  required: boolean;
+  customOnChange: any;
+  restProps: any;
 }
 
-const FileInput = ({
-   id,
-   name,
-   label = 'فایل خود را بارگذاری کنید',
-   type = 'file',
-   value,
-   onChange,
-   clear,
-   required = false,
-   touched,
-   onBlur,
-   error,
-   col,
-   isDisabled = false,
-   multiple = false,
-   customOnChange = () => {},
-}: Props) => {
-   let fileName = value ? value?.split('\\').pop() : false;
-   return (
-      <div className={`${col} h-full`}>
-         <label>
-            {label}
-            {required && <span className="text-sm text-red-600">*</span>}
-         </label>
-         <div
-            className={`${col} flex h-[38px] cursor-pointer items-center justify-between rounded-[4px] border dark:border-[#17263c] dark:bg-[#121e32]`}
-         >
-            <label htmlFor={id} className="m-0 cursor-pointer pr-3 text-sm font-normal text-[#757678]">
-               محتوای خود را بارگذاری کنید {fileName && `(${fileName})`}
-            </label>
-            {value && (
-               <button
-                  type="button"
-                  className="mx-2 my-2 inline-flex items-center rounded-lg bg-transparent text-sm text-gray-400 hover:bg-gray-600"
-                  data-modal-hide="medium-modal"
-                  onClick={() => clear(name, '')}
-               >
-                  <svg aria-hidden="true" className="h-5 w-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                     <path
-                        fillRule="evenodd"
-                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                        clipRule="evenodd"
-                     ></path>
-                  </svg>
-                  <span className="sr-only">Close modal</span>
-               </button>
-            )}
-         </div>
-         <p className="mt-1 text-xs text-red-500">{error && touched ? error : '\xa0'}</p>
-         <input
-            id={id}
-            name={name}
-            type={type}
-            value={value}
-            disabled={isDisabled}
-            multiple={multiple}
-            onChange={(e) => {
-               onChange(e);
-               customOnChange('content', e.target.files);
-            }}
-            onBlur={onBlur}
-            className="rtl:file-ml-5 form-input hidden p-0 file:border-0 file:bg-primary/90 file:px-4 file:py-2 file:font-semibold file:text-white file:hover:bg-primary ltr:file:mr-5"
-         />
-      </div>
-   );
+const FileInput = ({ id, label, name, control, error, required, customOnChange = () => {}, ...restProps }: Props) => {
+  const { field } = useController({
+    name,
+    control,
+    rules: { required },
+  });
+  return (
+    <>
+      <FormControl fullWidth size="small" required={required} error={error}>
+        <InputLabel id="demo-simple-select-label">{label}</InputLabel>
+        <TextField
+          // value={label}
+          inputProps={{
+            readOnly: true,
+          }}
+          fullWidth
+          InputProps={{
+            endAdornment: (
+              <label htmlFor={id}>
+                <InputAdornment position="end" className="cursor-pointer">
+                  <CloudUploadOutlinedIcon />
+                </InputAdornment>
+              </label>
+            ),
+          }}
+          size="small"
+          required={required}
+          error={error}
+          helperText={error?.type === "required" && "این فیلد اجباری است!"}
+        />
+      </FormControl>
+      <input
+        id={id}
+        type="file"
+        onChange={(e) => {
+          customOnChange(e.target.files);
+          field.onChange(e);
+        }} // send value to hook form
+        onBlur={field.onBlur} // notify when input is touched/blur
+        value={field.value || ""} // input value
+        name={field.name} // send down the input name
+        className="rtl:file-ml-5 form-input hidden p-0 file:border-0 file:bg-primary/90 file:px-4 file:py-2 file:font-semibold file:text-white file:hover:bg-primary ltr:file:mr-5"
+      />
+    </>
+  );
 };
 
-export default React.memo(FileInput);
+export default FileInput;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from 'next/app';
-import { ReactElement, ReactNode, Suspense, useEffect, useState } from 'react';
+import { ReactElement, ReactNode, Suspense, useEffect, useMemo, useState } from 'react';
 import DefaultLayout from '../components/Layouts/DefaultLayout';
 import { Provider } from 'react-redux';
 import store from '../store/index';
@@ -18,6 +18,11 @@ import { SToastContainer } from '../components/shared/notify/SNotify';
 import Loading from '@/components/shared/Loading';
 import { useRouter } from 'next/router';
 import api from '../services/interceptor';
+import rtlPlugin from 'stylis-plugin-rtl';
+import { prefixer } from 'stylis';
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
+import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 
 // const dana = localFont({
 //    src: [
@@ -79,18 +84,245 @@ type AppPropsWithLayout = AppProps & {
    Component: NextPageWithLayout;
 };
 
+const theme = () => {
+   let theme = createTheme({
+      direction: 'rtl',
+      typography: {
+         fontFamily: 'IRANSansX , sans-serif',
+         h1: {
+            fontSize: '1rem',
+            fontWeight: 600,
+         },
+         h5: {
+            fontSize: '1rem',
+            fontWeight: 600,
+            lineHeight: 1.5,
+         },
+         subtitle2: {
+            fontSize: '0.75rem',
+         },
+      },
+      palette: {
+         primary: {
+            main: '#225E82',
+            dark: '#1B4B68',
+            light: '#6A96B0',
+            contrastText: '#FFFFFF',
+         },
+         secondary: {
+            main: '#8AC43E',
+            dark: '#6E9D32',
+            light: '#A1D065',
+            contrastText: '#FFFFFF',
+         },
+         neutral: {
+            main: '#555859',
+            light: '#EDEDEE',
+            contrastText: '#FFFFFF',
+         },
+         info: {
+            main: '#0288D1',
+            dark: '#01579B',
+            light: '#03A9F4',
+            contrastText: '#FFFFFF',
+         },
+         success: {
+            main: '#2E7D32',
+            dark: '#1B5E20',
+            light: '#4CAF50',
+            contrastText: '#FFFFFF',
+         },
+         warning: {
+            main: '#EF6C00',
+            dark: '#EF6C00',
+            light: '#FF9800',
+            contrastText: '#FFFFFF',
+         },
+         error: {
+            main: '#D32F2F',
+            dark: '#C62828',
+            light: '#EF5350',
+            contrastText: '#FFFFFF',
+         },
+         background: {
+            // default: isDarkMode ? '#090909' : '#f8f8fd',
+         },
+      },
+      shape: {
+         borderRadius: 8,
+      },
+      mixins: {
+         toolbar: {
+            height: 64,
+         },
+         navbar: {
+            width: 278,
+         },
+      },
+   });
+
+   theme = createTheme(theme, {
+      components: {
+         MuiAppBar: {
+            defaultProps: {
+               variant: 'outlined',
+               elevation: 0,
+            },
+            styleOverrides: {
+               root: {
+                  borderWidth: '0 0 1px 0',
+               },
+               colorDefault: {
+                  backgroundColor: theme.palette.background.paper,
+               },
+            },
+         },
+         MuiCard: {
+            defaultProps: {
+               variant: 'outlined',
+            },
+            styleOverrides: {
+               root: {
+                  borderRadius: theme.shape.borderRadius * 2,
+               },
+            },
+         },
+         MuiCardContent: {
+            styleOverrides: {
+               root: {
+                  '&:last-child': {
+                     padding: theme.spacing(2),
+                  },
+               },
+            },
+         },
+         MuiListSubheader: {
+            styleOverrides: {
+               root: {
+                  fontSize: '0.75rem',
+                  lineHeight: 2,
+                  marginBottom: theme.spacing(1),
+               },
+            },
+         },
+         MuiListItem: {
+            styleOverrides: {
+               root: {
+                  padding: theme.spacing(1),
+               },
+            },
+         },
+         MuiListItemSecondaryAction: {
+            styleOverrides: {
+               root: {
+                  right: theme.spacing(1),
+                  '& svg': {
+                     width: 20,
+                  },
+               },
+            },
+         },
+         MuiListItemButton: {
+            styleOverrides: {
+               root: {
+                  borderRadius: theme.shape.borderRadius,
+                  padding: theme.spacing(0),
+                  '&.Mui-selected': {
+                     color: theme.palette.primary.contrastText,
+                     backgroundColor: theme.palette.primary.main,
+                  },
+                  '&.sub-selected': {
+                     color: theme.palette.primary.main,
+                     borderLeft: `2px solid ${theme.palette.primary.main}`,
+                  },
+               },
+            },
+         },
+         MuiListItemText: {
+            styleOverrides: {
+               root: {
+                  margin: 0,
+               },
+            },
+         },
+         MuiListItemIcon: {
+            styleOverrides: {
+               root: {
+                  minWidth: 32,
+                  color: 'inherit',
+                  '& svg': {
+                     width: 20,
+                  },
+               },
+            },
+         },
+         MuiButton: {
+            defaultProps: {
+               variant: 'contained',
+               disableElevation: true,
+            },
+            styleOverrides: {
+               root: {
+                  minWidth: 42,
+                  padding: theme.spacing(1, 2),
+                  borderRadius: theme.shape.borderRadius * 1.5,
+                  lineHeight: 1.5,
+                  '& svg': {
+                     width: '1rem',
+                  },
+                  '&.square': {
+                     padding: theme.spacing(1),
+                  },
+               },
+               outlinedNeutral: {
+                  border: `1px solid ${theme.palette.neutral.main}33`,
+               },
+            },
+         },
+         MuiTab: {
+            styleOverrides: {
+               root: {
+                  fontSize: '0.95rem',
+                  color: theme.palette.primary.dark,
+                  minHeight: 50,
+               },
+            },
+         },
+         MuiDialog: {
+            styleOverrides: {
+               paper: {
+                  borderRadius: theme.shape.borderRadius * 2,
+               },
+            },
+         },
+         MuiDialogTitle: {
+            styleOverrides: {
+               root: {
+                  fontSize: '1rem',
+                  fontWeight: 600,
+                  display: 'flex',
+                  gap: theme.spacing(1),
+                  alignItems: 'center',
+                  '& span': {
+                     flexGrow: 1,
+                  },
+               },
+            },
+         },
+      },
+   });
+   return theme;
+};
+
+const cacheRtl = createCache({
+   key: 'muirtl',
+   stylisPlugins: [prefixer, rtlPlugin],
+});
+
 const App = ({ Component, pageProps }: AppPropsWithLayout) => {
    const [handleShow, setHandleShow] = useState(false);
    const getLayout = Component.getLayout ?? ((page) => <DefaultLayout>{page}</DefaultLayout>);
-
-   // const { data, isLoading, error } = useAuthUser();
    const router = useRouter();
-
-   // if (isLoading) return <Loading title={'در حال احراز هویت '} />;
-
-   // if (error) {
-   //    if (!router.asPath.startsWith('/auth')) router.push('/auth/login');
-   // }
 
    useEffect(() => {
       setTimeout(() => {
@@ -105,20 +337,25 @@ const App = ({ Component, pageProps }: AppPropsWithLayout) => {
    if (!handleShow) {
       return <Loading title={'در حال احراز هویت '} />;
    }
+
    return (
       <Provider store={store}>
-         <Head>
-            <title>VRISTO - Multipurpose Tailwind Dashboard Template</title>
-            <meta charSet="UTF-8" />
-            <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-            <meta name="description" content="Generated by create next app" />
-            <link rel="icon" href="/favicon.png" />
-         </Head>
-         <main className={`${iranSans.variable}`}>
-            <SToastContainer />
-            {getLayout(<Component {...pageProps} />)}
-         </main>
+         <CacheProvider value={cacheRtl}>
+            <ThemeProvider theme={theme}>
+               <Head>
+                  <title>VRISTO - Multipurpose Tailwind Dashboard Template</title>
+                  <meta charSet="UTF-8" />
+                  <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                  <meta name="description" content="Generated by create next app" />
+                  <link rel="icon" href="/favicon.png" />
+               </Head>
+               <main className={`${iranSans.variable}`}>
+                  <SToastContainer />
+                  {getLayout(<Component {...pageProps} />)}
+               </main>
+            </ThemeProvider>
+         </CacheProvider>
       </Provider>
    );
 };
